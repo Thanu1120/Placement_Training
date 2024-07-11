fd4593bf7ed24831f6057b959369f371638eadab
Thanu1120 <thanushreedevi2003@gmail.com>
2024-07-11 14:47:06 +0530

    Placement Training Assessments

---
+++ Week3/Day12/p1/area.js
@@ -0,0 +1,17 @@
+function calculateArea() {
+    var shape = prompt("Enter Shape");
+  shape=shape.toLowerCase();
+    var area = 0;
+    if (shape == "circle") {
+      var radius = prompt("Enter radius");
+      area = Math.PI * Math.pow(radius, 2);
+      document.
+    }
+    if (shape == "rectangle") {
+      var width = prompt("Enter width");
+      var length = prompt("Enter width");
+      area = Math.PI * width * length;
+    }
+    return area;
+  }
+  calculateArea();
